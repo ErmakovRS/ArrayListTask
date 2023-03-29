34e570f85dd5ec894f4be99b954ae41c6d2ba04b
ErmakovRS <rermakov777@gmail.com>
2023-03-29 20:20:10 +0300

    SWEQWEWQ

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+public static void main(String[]args){
+    ArrayListCreation arrayListCreation = new ArrayListCreation();
+
+    arrayListCreation.sortArrayList();
+
+        }
+}
